b1d525676a670398fea7256e25a7c202e2aa9f66
xiong.bob@gmail.com <xiong.bob@gmail.com>
2019-05-24 11:10:01 -0400

    whatever-commit-Step2

---
+++ src/main/java/com/example/demo/BasicBullhorn2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBullhorn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicBullhorn2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,72 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=4)
+    private String subject;
+
+    @NotNull
+    @Size(min=5)
+    private String content;
+
+    @NotNull
+    @Size(min=8)
+    private String posteddate;
+
+    @NotNull
+    @Size(min=10)
+    private String sentby;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPosteddate() {
+        return posteddate;
+    }
+
+    public void setPosteddate(String posteddate) {
+        this.posteddate = posteddate;
+    }
+
+    public String getSentby() {
+        return sentby;
+    }
+
+    public void setSentby(String sentby) {
+        this.sentby = sentby;
+    }
+}
